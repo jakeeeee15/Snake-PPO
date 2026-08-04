aa5488a54ac363f1f59ab9d5bb5073b1d199c85b
Jake John George <jakejohngeorge22@gmail.com>
2026-08-05 01:50:15 +0530

    made the neural network architecture

--- playing_model.py
+++ playing_model.py
@@ -1,1 +1,50 @@
 import torch
+from torch import nn
+from torch.nn import Module
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+
+class PpoModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(12, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+
+        flatten_size = 13*13*64
+
+        self.shared_linear = nn.Linear(flatten_size, 256)
+
+        self.actor_hidden = nn.Linear(256, 128)
+        self.actor_out_layer = nn.Linear(128, 4)
+
+        self.critic_hidden = nn.Linear(256, 128)
+        self.critic_out_layer = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        x = F.relu(self.shared_linear(x))
+
+        actor_x = F.relu(self.actor_hidden(x))
+        actor_out = self.actor_out_layer(actor_x)
+
+        critic_x = F.relu(self.critic_hidden(x))
+        state_value = self.critic_out_layer(critic_x)
+
+        return actor_out, state_value
+
+    def get_action(self, state):
+        action, value = self.forward(state)
+        action_dist = Categorical(logits=action)
+
+        action_chosen = action_dist.sample()
+        return action_chosen.item(), value, action_dist.log_prob(action_chosen), action_dist.entropy()
+
+
+
+
+#make the frame stacker
